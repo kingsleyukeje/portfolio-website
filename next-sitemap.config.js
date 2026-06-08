a708fd95f04ee5f1756f26d41104430a7bf3d500
PeluMike <pmichael3006@gmail.com>
2026-06-08 12:41:38 +0100

    seo optimization started

---
+++ next-sitemap.config.js
@@ -0,0 +1,27 @@
+
+
+// 
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://kingsleyukeje.com",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ["/fonts"],
+      },
+    ]
+  },
+  exclude: ["/old"],
+  additionalPaths: async (config) => {
+    return [
+      // await config.transform(config, '/', { changefreq: 'daily', priority: 1.0 }),        
+      await config.transform(config, '/', { changefreq: 'daily', priority: 1 }),   
+      // Add other pages you want to include
+    ];
+  },
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
+/** @format */
+
 import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import AppLoaderWrapper from "@/components/AppLoaderWrapper";
+import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -13,7 +16,7 @@ const roboto_mono = Roboto_Mono({
   display: "swap",
   subsets: ["latin"],
   variable: "--font-roboto-mono",
-})
+});
 
 export const metadata: Metadata = {
   title: "Kingsley Ukeje - Portfolio",
@@ -49,6 +52,58 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${roboto_mono.variable}`}>
+      <Script
+        id="person-schema"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Person",
+            name: "Kingsley Ukeje",
+            url: "https://kingsleyukeje.com",
+            jobTitle: "UX Engineer and Principal Product Designer",
+            worksFor: [
+              {
+                "@type": "Organization",
+                name: "Eze (YC W20)",
+                url: "https://www.ezewholesale.com",
+              },
+              {
+                "@type": "Organization",
+                name: "Carrot",
+                url: "https://www.carrotcredit.com",
+              },
+              {
+                "@type": "Organization",
+                name: "Hotels.ng",
+                url: "https://hotels.ng",
+              },
+              {
+                "@type": "Organization",
+                name: "Space",
+                website: "",
+              },
+            ],
+            sameAs: [
+              "https://x.com/kingsleyukeje_",
+              "https://www.linkedin.com/in/kingsleyukeje",
+              "https://www.youtube.com/@kingsleyukeje",
+              "https://kingsleyukeje.substack.com",
+              "https://www.are.na/kingsley-ukeje/channels",
+            ],
+            subjectOf: [
+              {
+                "@type": "Article",
+                url: "https://www.vanguardngr.com/2023/12/kingsley-ukeje-empowering-the-next-generation-of-african-tech-talent",
+              },
+              {
+                "@type": "Article",
+                url: "https://www.naijanews.com/2024/05/06/from-market-stalls-to-silicon-valley-how-the-igbo-apprenticeship-system-can-fuel-the-next-generation-of-tech-innovators-kingsley-ukeje",
+              },
+            ],
+          }),
+        }}
+      />
       <body className="bg-white text-black dark:bg-dark-bg dark:text-white antialiased transition-colors duration-500">
         <AppLoaderWrapper>{children}</AppLoaderWrapper>
       </body>
